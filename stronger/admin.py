d819e5022024c6153c50a1f1bd2624f055e5f047
Danny Milsom <dannysmilsom@gmail.com>
2015-05-02 15:46:06 +0100

    Making the tuple of admin modules more readable

--- stronger/admin.py
+++ stronger/admin.py
@@ -3,5 +3,12 @@ from django.contrib import admin
 from .models import (Friend, Group, GroupMember, Workout, Set, 
                      Exercise, BodyWeight, DailyNutrition)
 
-admin.site.register((Friend, Group, GroupMember, Workout, 
-                     Set, Exercise, BodyWeight, DailyNutrition))
+admin.site.register((Friend,
+                     Group,
+                     GroupMember,
+                     Workout, 
+                     Set,
+                     Exercise,
+                     BodyWeight,
+                     DailyNutrition,
+                   ))
