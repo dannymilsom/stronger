a53e13bc8c1fa598e3dace996eb253d27a0465ca
Danny Milsom <dannysmilsom@gmail.com>
2016-01-05 20:19:29 +0000

    Move all nested script tags in child templates into a script block

--- src/stronger/tests/test_models.py
+++ src/stronger/tests/test_models.py
@@ -49,3 +49,78 @@ class TestBodyweightModel(TestCase):
             self.danny_bodyweights[0]
         )
 
+
+class TestSelectRealted(TestCase):
+
+    def setUp(self):
+
+        from ..models import Exercise, Workout, Set
+
+        self.danny = UserFactory(username='danny')
+
+        self.rows = Exercise.objects.create(
+            name='barbell rows',
+            clean_name='barbellrows',
+            primary_muscle='Back',
+            secondary_muscles='Biceps',
+            added_by=self.danny,
+        )
+
+        workout1 = Workout.objects.create(
+            user=self.danny,
+            date=timezone.now().date(),
+            description='qqqq',
+            comments='dsfdsfsdf'
+        )
+
+        workout2 = Workout.objects.create(
+            user=self.danny,
+            date=timezone.now().date(),
+            description='wwww',
+            comments='dfgfdg'
+        )
+
+        set1 = Set.objects.create(
+            workout=workout1,
+            exercise=self.rows,
+            weight=100,
+            reps=10
+        )
+
+        set2 = Set.objects.create(
+            workout=workout1,
+            exercise=self.rows,
+            weight=90,
+            reps=5
+        )
+
+        set3 = Set.objects.create(
+            workout=workout2,
+            exercise=self.rows,
+            weight=110,
+            reps=10
+        )
+
+        set2 = Set.objects.create(
+            workout=workout1,
+            exercise=self.rows,
+            weight=120,
+            reps=5
+        )
+
+
+    def test_raw_query_count(self):
+        from ..models import Set
+        with self.assertNumQueries(5):
+            [s.workout for s in Set.objects.filter(exercise=self.rows)]
+
+    def test_values_list_query_count(self):
+        from ..models import Set
+        with self.assertNumQueries(1):
+            [s for s in Set.objects.filter(exercise=self.rows).values_list('workout', flat=True)]
+
+    def test_select_related_query_count(self):
+        from ..models import Set
+        with self.assertNumQueries(1):
+            [s for s in Set.objects.filter(exercise=self.rows).select_related('workout').values_list('workout', flat=True)]
+
